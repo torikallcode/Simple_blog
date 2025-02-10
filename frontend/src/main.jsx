51bd2aed14feaa01222e19dca546882722119e11
torikallcode <torikalcontent@gmail.com>
2025-02-10 10:02:39 +0800

    update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,14 +2,30 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import { Home } from './pages/home'
+import { ArticleList } from './components/ArticleList'
+import { ArticleForm } from './components/ArticleForm'
+import { ArticleDetail } from './components/ArticleDetail'
+import axios from 'axios'
 
+const handleCreate = (article) => {
+  axios.post('http://localhost:8080/articles', article)
+    .then(response => console.log(response.data))
+    .catch(error => console.error(error));
+};
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
+    element: <ArticleList />,
   },
+  {
+    path: "/articles/new",
+    element: <ArticleForm onSubmit={handleCreate} />
+  },
+  {
+    path: "/articles/:id",
+    element: <ArticleDetail />
+  }
 ])
 createRoot(document.getElementById('root')).render(
   <StrictMode>
