020fc434dea55ce1c9a9f2a0d479aaa15c03c3ae
torikallcode <torikalcontent@gmail.com>
2025-02-14 21:35:34 +0800

    create connection database

---
+++ backend/database/connection.go
@@ -0,0 +1,26 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+)
+
+var DB *sql.DB
+
+func InitDatabase() {
+	connectionString := "root:torikal@tcp(localhost:3306)/simple_blog"
+
+	db, err := sql.Open("mysql", connectionString)
+	if err != nil {
+		log.Fatal("Error connection to database", err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal("Error ping database", err)
+	}
+
+	DB = db
+	fmt.Println("Database Connection sucessfully!")
+}
